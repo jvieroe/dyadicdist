27daed173013bee9f6d211d73be17499e30f3221
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-10-04 13:50:05 +0200

    ddist

--- R/ddist.R
+++ R/ddist.R
@@ -1,10 +1,8 @@
 #' Create dyads of points i and j and calculate distances
 #'
 #' This function calculates the geodesic distance between any dyads (pairs of points) and stores the result in a long tibble, a opposed to a wide matrix
-#' @param data a data.frame or tibble.
-#' @param id a column with a numeric indicator uniquely idenfiying points.
-#' @param crs a valid EPSG for a valid Coordinate Reference System (CRS). Defaults to 4326.
-#' @return ... x
+#' @param ... x
+#' @return ... y
 #' @author Jeppe Vierø
 #' @export
 
