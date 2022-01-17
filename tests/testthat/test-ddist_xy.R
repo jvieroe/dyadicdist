60a7223611358214b1f4bd999e858cc82a212dfe
Jeppe Vierø <43220957+jvieroe@users.noreply.github.com>
2022-01-17 11:28:11 +0100

    tests

--- tests/testthat/test-ddist_xy.R
+++ tests/testthat/test-ddist_xy.R
@@ -18,54 +18,19 @@ test_that("output dimensions work", {
 })
 
 
-
-test_that("output dimensions work", {
-  expect_equal(nrow(
-    dyadicdist::ddist_xy(x = df_1,
-                         y = df_2,
-                         ids = c("id", "id"))
-    ),
-    nrow(df_1) * nrow(df_2))
-})
-
-
-test_that("output dimensions work", {
-  expect_equal(nrow(dyadicdist::ddist_sf(df,
-                                         id = "id",
-                                         diagonal = FALSE,
-                                         duplicates = TRUE)),
-               (nrow(df) * nrow(df)) - nrow(df))
-})
-
-
-test_that("output dimensions work", {
-  expect_equal(nrow(dyadicdist::ddist_sf(df,
-                                         id = "id",
-                                         diagonal = TRUE,
-                                         duplicates = FALSE)),
-               (nrow(df)*nrow(df))/2+(nrow(df)/2))
-})
-
-test_that("output dimensions work", {
-  expect_equal(nrow(dyadicdist::ddist_sf(df,
-                                         id = "id",
-                                         diagonal = FALSE,
-                                         duplicates = FALSE)),
-               (nrow(df)*nrow(df))/2-(nrow(df)/2))
-})
-
-
 test_that("distance units work", {
-  expect_equal(dyadicdist::ddist_sf(df,
-                                    id = "id") %>%
+  expect_equal(dyadicdist::ddist_xy(ca,
+                                    tx,
+                                    ids = c("id", "id")) %>%
                  pull(distance_units) %>%
                  unique(),
                "m")
 })
 
 test_that("distance units work", {
-  expect_equal(dyadicdist::ddist_sf(df,
-                                    id = "id",
+  expect_equal(dyadicdist::ddist_xy(ca,
+                                    tx,
+                                    ids = c("id", "id"),
                                     crs_transform = TRUE,
                                     new_crs = 3359) %>%
                  pull(distance_units) %>%
--- tests/testthat/test-ddist_xy_sf.R
+++ tests/testthat/test-ddist_xy_sf.R
@@ -8,62 +8,36 @@ df <- df %>%
   sf::st_as_sf(coords = c("longitude", "latitude"),
                crs = 4326)
 
-df_1 <- df %>%
+ca <- df %>%
   filter(state == "CA")
 
-df_2 <- df %>%
+tx <- df %>%
   filter(state == "TX")
 
 
 test_that("output dimensions work", {
-  expect_equal(nrow(dyadicdist::ddist_xy_sf(x = df_1,
-                                            y = df_2,
-                                            ids = c("id", "id")
-                                            )
-                    ),
-               nrow(df_1) * nrow(df_2))
-})
-
-
-test_that("output dimensions work", {
-  expect_equal(nrow(dyadicdist::ddist_sf(df,
-                                         id = "id",
-                                         diagonal = FALSE,
-                                         duplicates = TRUE)),
-               (nrow(df) * nrow(df)) - nrow(df))
-})
-
-
-test_that("output dimensions work", {
-  expect_equal(nrow(dyadicdist::ddist_sf(df,
-                                         id = "id",
-                                         diagonal = TRUE,
-                                         duplicates = FALSE)),
-               (nrow(df)*nrow(df))/2+(nrow(df)/2))
-})
-
-test_that("output dimensions work", {
-  expect_equal(nrow(dyadicdist::ddist_sf(df,
-                                         id = "id",
-                                         diagonal = FALSE,
-                                         duplicates = FALSE)),
-               (nrow(df)*nrow(df))/2-(nrow(df)/2))
+  expect_equal(nrow(dyadicdist::ddist_xy_sf(ca,
+                                            tx,
+                                            ids = c("id", "id"))),
+               nrow(ca) * nrow(tx))
 })
 
 
 test_that("distance units work", {
-  expect_equal(dyadicdist::ddist_sf(df,
-                                    id = "id") %>%
+  expect_equal(dyadicdist::ddist_xy_sf(ca,
+                                       tx,
+                                       ids = c("id", "id")) %>%
                  pull(distance_units) %>%
                  unique(),
                "m")
 })
 
 test_that("distance units work", {
-  expect_equal(dyadicdist::ddist_sf(df,
-                                    id = "id",
-                                    crs_transform = TRUE,
-                                    new_crs = 3359) %>%
+  expect_equal(dyadicdist::ddist_xy_sf(ca,
+                                       tx,
+                                       ids = c("id", "id"),
+                                       crs_transform = TRUE,
+                                       new_crs = 3359) %>%
                  pull(distance_units) %>%
                  unique(),
                "US_survey_foot")
