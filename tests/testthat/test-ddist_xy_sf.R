9fbd23beb02e0a1ee5941f718bef9d945ae9242c
Jeppe Vierø <43220957+jvieroe@users.noreply.github.com>
2022-01-17 11:17:31 +0100

    xy

--- tests/testthat/test-ddist_xy_sf.R
+++ tests/testthat/test-ddist_xy_sf.R
@@ -11,5 +11,60 @@ df <- df %>%
 df_1 <- df %>%
   filter(state == "CA")
 
-df_2 <- df_sf %>%
+df_2 <- df %>%
   filter(state == "TX")
+
+
+test_that("output dimensions work", {
+  expect_equal(nrow(dyadicdist::ddist_xy_sf(x = df_1,
+                                            y = df_2,
+                                            ids = c("id", "id")
+                                            )
+                    ),
+               nrow(df_1) * nrow(df_2))
+})
+
+
+test_that("output dimensions work", {
+  expect_equal(nrow(dyadicdist::ddist_sf(df,
+                                         id = "id",
+                                         diagonal = FALSE,
+                                         duplicates = TRUE)),
+               (nrow(df) * nrow(df)) - nrow(df))
+})
+
+
+test_that("output dimensions work", {
+  expect_equal(nrow(dyadicdist::ddist_sf(df,
+                                         id = "id",
+                                         diagonal = TRUE,
+                                         duplicates = FALSE)),
+               (nrow(df)*nrow(df))/2+(nrow(df)/2))
+})
+
+test_that("output dimensions work", {
+  expect_equal(nrow(dyadicdist::ddist_sf(df,
+                                         id = "id",
+                                         diagonal = FALSE,
+                                         duplicates = FALSE)),
+               (nrow(df)*nrow(df))/2-(nrow(df)/2))
+})
+
+
+test_that("distance units work", {
+  expect_equal(dyadicdist::ddist_sf(df,
+                                    id = "id") %>%
+                 pull(distance_units) %>%
+                 unique(),
+               "m")
+})
+
+test_that("distance units work", {
+  expect_equal(dyadicdist::ddist_sf(df,
+                                    id = "id",
+                                    crs_transform = TRUE,
+                                    new_crs = 3359) %>%
+                 pull(distance_units) %>%
+                 unique(),
+               "US_survey_foot")
+})
